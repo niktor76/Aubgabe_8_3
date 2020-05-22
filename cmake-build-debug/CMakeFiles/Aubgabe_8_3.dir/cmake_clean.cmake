6ad12a4740c142e9965fe3f28406f12656c03a5d
viktor-nbubar <viktor.steck@gmail.com>
2020-05-22 22:54:46 +0200

    And so, it begins.

---
+++ cmake-build-debug/CMakeFiles/Aubgabe_8_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Aubgabe_8_3"
+  "Aubgabe_8_3.pdb"
+  "CMakeFiles/Aubgabe_8_3.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Aubgabe_8_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
